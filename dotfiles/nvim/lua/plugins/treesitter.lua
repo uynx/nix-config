bdf33119295c9d2e85cbd8af464a83d3e596ff8a
Brandon Alexander <brandonwalex@pm.me>
2026-02-25 18:12:26 -0500

    Clean up config files.

--- dotfiles/nvim/lua/plugins/treesitter.lua
+++ dotfiles/nvim/lua/plugins/treesitter.lua
@@ -3,21 +3,6 @@ return {
   opts = function(_, opts)
     if type(opts.ensure_installed) == "table" then
       vim.list_extend(opts.ensure_installed, {
-        "bash",
-        "html",
-        "javascript",
-        "json",
-        "lua",
-        "markdown",
-        "markdown_inline",
-        "python",
-        "query",
-        "regex",
-        "tsx",
-        "typescript",
-        "vim",
-        "vimdoc",
-        "yaml",
         "css",
         "latex",
         "scss",
