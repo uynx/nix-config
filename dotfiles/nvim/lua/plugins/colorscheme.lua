a78b867f635b3cb8268cf1030c59723d27d24bd4
Brandon Alexander <brandonwalex@pm.me>
2026-03-02 13:50:42 -0500

    Firenvim fix

--- dotfiles/nvim/lua/plugins/colorscheme.lua
+++ dotfiles/nvim/lua/plugins/colorscheme.lua
@@ -1,10 +1,8 @@
 return {
-  -- Install the official Flexoki theme plugin by kepano
   {
     "kepano/flexoki-neovim",
     name = "flexoki",
   },
-  -- Configure LazyVim to use it as the default colorscheme
   {
     "LazyVim/LazyVim",
     opts = {
