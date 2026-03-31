4768db577cffc7d1f3f12b380e2a9f752ecd7b4e
Brandon Alexander <brandonwalex@pm.me>
2026-03-31 12:45:34 -0400

    Updates

--- dotfiles/nvim/lua/plugins/vimtex.lua
+++ dotfiles/nvim/lua/plugins/vimtex.lua
@@ -1,17 +1,13 @@
 return {
   "lervag/vimtex",
-  lazy = false, -- VimTeX should not be lazy loaded for best functionality
-  config = function()
-    -- Set the viewer to Sioyek
+  lazy = false,
+  init = function()
     vim.g.vimtex_view_method = "sioyek"
-    
-    -- Configure the compiler (using latexmk, which you have via Nix)
     vim.g.vimtex_compiler_method = "latexmk"
-    
-    -- Extra compiler options (optional, but recommended for clean builds)
+    vim.g.vimtex_compiler_latexmk_engine = "lualatex"
     vim.g.vimtex_compiler_latexmk = {
       options = {
-        "-pdflua", -- Use LuaLaTeX instead of pdfLaTeX
+        "-pdflua",
         "-shell-escape",
         "-verbose",
         "-file-line-error",
@@ -19,8 +15,6 @@ return {
         "-interaction=nonstopmode",
       },
     }
-
-    -- This setting ensures inverse search works out of the box with Neovim
     vim.g.vimtex_callback_progname = "nvim"
   end,
 }
