1f15c0d94348f6d85a065fd6a74a708b3a217341
Brandon Alexander <brandonwalex@pm.me>
2026-04-08 13:25:22 -0400

    Package Updates.

--- darwin.nix
+++ darwin.nix
@@ -8,8 +8,6 @@
 {
   users.users.uynx.home = "/Users/uynx";
 
-  nix.enable = false;
-
   determinateNix = {
     enable = true;
     determinateNixd = {
@@ -17,6 +15,24 @@
     };
     customSettings = {
       auto-optimise-store = true;
+      experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
+      trusted-users = [
+        "root"
+        "uynx"
+      ];
+      substituters = [
+        "https://cache.nixos.org"
+        "https://nix-community.cachix.org"
+        "https://numtide.cachix.org"
+      ];
+      trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "numtide.cachix.org-1:2ps1kLBUWnL9yCkD69XfYIa2VclDuxsBeE266mGrW0o="
+      ];
     };
   };
 
@@ -74,7 +90,6 @@
         "com.apple.Safari" = {
           UniversalSearchEnabled = false;
           PreloadTopHit = false;
-          SendDoNotTrackHTTPHeader = true;
           BlockStoragePolicy = 2;
           IncludeInternalDebugMenu = true;
           IncludeDevelopMenu = true;
